3cf8f918972e66d12a2c93a360125faf3c41301d
Juniel Filappi <juniel.filappi@crmpiperun.com>
2024-05-22 09:42:31 -0400

    first commit

---
+++ React/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,12 @@
+import {Post} from "./Post.jsx";
+
+export function App() {
+    return (
+        <div>
+            <Post
+                author="John Doe"
+                content="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            />
+        </div>
+    )
+}
