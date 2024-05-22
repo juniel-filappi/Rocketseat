91e4a9b4886e34d3bfb298c26405386618682808
Juniel Filappi <juniel.filappi@crmpiperun.com>
2024-05-22 17:07:55 -0400

    feature: comment

---
+++ React/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import styles from './Comment.module.css';
+import {ThumbsUp, Trash} from "phosphor-react";
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/juniel-filappi.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Juniel Filappi</strong>
+                            <time
+                                title="11 de maio ás 08:12"
+                                dateTime="2022-05-11 08:12:30"
+                            >
+                                Cerca de 1 hora atrás
+                            </time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Teste, parabéns!!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
