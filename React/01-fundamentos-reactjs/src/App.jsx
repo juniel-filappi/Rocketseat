7f09eec8043eaeea83ddd745e22020e2540af9bb
Juniel Filappi <juniel.filappi@crmpiperun.com>
2024-05-22 14:45:12 -0400

    02 started

--- React/01-fundamentos-reactjs/src/App.jsx
+++ React/01-fundamentos-reactjs/src/App.jsx
@@ -1,12 +1,27 @@
-import {Post} from "./Post.jsx";
+import {Post} from "./components/Post.jsx";
+import {Header} from "./components/Header.jsx";
+import './global.css'
+import styles from './App.module.css'
+import {Sidebar} from "./components/Sidebar.jsx";
 
 export function App() {
     return (
         <div>
-            <Post
-                author="John Doe"
-                content="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-            />
+            <Header/>
+
+            <div className={styles.wrapper}>
+                <Sidebar />
+                <main>
+                    <Post
+                        author="Lucas Nhimi"
+                        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    />
+                    <Post
+                        author="Lucas Nhimi"
+                        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    />
+                </main>
+            </div>
         </div>
     )
 }
---
+++ React/01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,42 @@
+import styles from './Post.module.css'
+
+export function Post(props) {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img
+                        className={styles.avatar}
+                        src="https://github.com/juniel-filappi.png"
+                    />
+                    <div className={styles.authorInfo}>
+                        <strong>John Doe</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time
+                    title="11 de maio ás 08:12"
+                    dateTime="2022-05-11 08:12:30"
+                >
+                    Publicado há 1h
+                </time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da
+                    Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+                <p>👉<a href="">jane.design/doctorcare</a></p>
+
+                <p>
+                    <a href="">#novoprojeto</a>{' '}
+                    <a href="">#nlw</a>{' '}
+                    <a href="">#rocketseat</a>
+                </p>
+            </div>
+        </article>
+    )
+}
