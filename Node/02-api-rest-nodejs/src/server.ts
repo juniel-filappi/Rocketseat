df677d0b820e979130bb9c5948e3fd2b13c60c6a
Juniel Filappi <juniel.filappi@crmpiperun.com>
2024-05-27 20:25:50 -0400

    level 2

--- Node/02-api-rest-nodejs/src/server.ts
+++ Node/02-api-rest-nodejs/src/server.ts
@@ -1,13 +1,19 @@
 import fastify from 'fastify'
+import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-    return {hello: 'world'}
+  const tables = knex('sqlite_schema').select('*')
+
+  return tables
 })
 
-app.listen({
-    port: 3333
-}).then(() => {
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
     console.log('Server is running on port 3333')
-})
+  })
