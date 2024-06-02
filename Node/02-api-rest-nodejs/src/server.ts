ca974dabffafff540325cf17111c4d92f253843f
Juniel Filappi <juniel.filappi@crmpiperun.com>
2024-06-02 17:30:51 -0400

    level 2

--- Node/02-api-rest-nodejs/src/server.ts
+++ Node/02-api-rest-nodejs/src/server.ts
@@ -1,14 +1,5 @@
-import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env'
-
-const app = fastify()
-
-app.get('/hello', async () => {
-  const tables = knex('sqlite_schema').select('*')
-
-  return tables
-})
+import { app } from './app'
 
 app
   .listen({
