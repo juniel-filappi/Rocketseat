9933f8d58cbfed739746e5778a1377e24dab253e
Juniel Filappi <juniel123123@outlook.com>
2024-12-15 22:36:56 -0400

    feature: delete question comment

--- Node/05-nest-clean/src/domain/forum/application/use-cases/delete-question-comment.ts
+++ Node/05-nest-clean/src/domain/forum/application/use-cases/delete-question-comment.ts
@@ -2,6 +2,7 @@ import { QuestionCommentsRepository } from '@/domain/forum/application/repositor
 import { Either, left, right } from '@/core/either'
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
 import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+import { Injectable } from "@nestjs/common";
 
 interface DeleteQuestionCommentUseCaseRequest {
   authorId: string
@@ -13,6 +14,7 @@ type DeleteQuestionCommentUseCaseResponse = Either<
   null
 >
 
+@Injectable()
 export class DeleteQuestionCommentUseCase {
   constructor(private questionCommentRepository: QuestionCommentsRepository) {}
 
